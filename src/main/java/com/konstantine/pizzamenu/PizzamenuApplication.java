3e2fe113f83be799eb04204345de5880f093430d
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-04-02 15:51:08 +0200

    Initial commit

---
+++ src/main/java/com/konstantine/pizzamenu/PizzamenuApplication.java
@@ -0,0 +1,13 @@
+package com.konstantine.pizzamenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzamenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzamenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konstantine/pizzamenu/PizzamenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konstantine.pizzamenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzamenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
