58f5175b3327a7ec7e736aa62d45e3cd0677487e
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-04-02 17:07:50 +0200

    Update AnnotationConfigApplicationContext in main

--- src/main/java/com/konstantine/pizzamenu/PizzamenuApplication.java
+++ src/main/java/com/konstantine/pizzamenu/PizzamenuApplication.java
@@ -1,7 +1,9 @@
 package com.konstantine.pizzamenu;
 
+import com.konstantine.pizzamenu.entities.Menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 
@@ -9,6 +11,9 @@ import org.springframework.boot.CommandLineRunner;
 public class PizzamenuApplication {
 
 	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(PizzamenuApplication.class);
+				Menu menu = ctx.getBean(Menu.class);
+				        menu.stampaMenu();
 	}
 
 
